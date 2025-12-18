26e671c838019e0a22aca6b772beee4e53082729
Almas-bo <252100@astanait.edu.kz>
2025-12-18 17:41:35 +0500

    final.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,72 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+class Room {
+    int number;
+    String type;
+    boolean occupied;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    Room(int n, String t, boolean o) {
+        number = n;
+        type = t;
+        occupied = o;
+    }
+
+    void show() {
+        System.out.println("Room " + number + " type: " + type);
+        if (occupied) {
+            System.out.println("Status: occupied");
+        } else {
+            System.out.println("Status: free");
+        }
+    }
+}
+
+class Guest {
+    String name;
+    int points;
+
+    Guest(String n, int p) {
+        name = n;
+        points = p;
+    }
+
+    void show() {
+        System.out.println("Guest: " + name);
+        if (points > 100) {
+            System.out.println("VIP guest");
+        } else {
+            System.out.println("Normal guest");
+        }
+    }
+}
+
+class Booking {
+    String id;
+    int nights;
+
+    Booking(String i, int n) {
+        id = i;
+        nights = n;
+    }
+
+    void show() {
+        System.out.println("Booking id: " + id);
+        System.out.println("Nights: " + nights);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Room r = new Room(101, "Deluxe", false);
+        r.show();
+
+        System.out.println();
+
+        Guest g = new Guest("Almas", 120);
+        g.show();
+
+        System.out.println();
+
+        Booking b = new Booking("B1", 3);
+        b.show();
     }
 }
